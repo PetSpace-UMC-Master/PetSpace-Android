1c7a17a06ce9e2ec46bf7123524e10cf844b9768
Madeline <jyseen@naver.com>
2023-01-05 16:29:44 +0900

    {FIX} 더보기 버튼 & 수정

--- app/src/main/java/com/example/petspace3/MainActivity.kt
+++ app/src/main/java/com/example/petspace3/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.CompoundButton
 import com.example.petspace3.databinding.ActivityMainBinding
 import kr.co.prnd.readmore.ReadMoreTextView
 
@@ -18,16 +17,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //개인 정보 수집 동의 내용 (tvAgreeContent1)
-        binding.tvAgreeContent1.text = "1. 펫스페이스가 수집하는 개인정보\n" +
-                "품고 기관과 있으며, 만물은 곳으로 운다. 사랑의 그들은 같이,\n" +
-                "청춘에서만 되려니와, 방지하는 있는가? 그들을 실현에 유소년\n" +
-                "에게서 봄바람이다. 따뜻한 바로 영원히 우리 커다란 그들은 위\n" +
-                "하여 없으면, 이상을 봄바람이다. 불어 구하기 같이, 사막이다.\n" +
-                "품고 기관과 있으며, 만물은 곳으로 운다. 사랑의 그들은 같이,\n" +
-                "청춘에서만 되려니와, 방지하는 있는가? 그들을 실현에 유소년\n" +
-                "에게서 봄바람이다. 따뜻한 바로 영원히 우리 커다란..."
 
-        //더보기 클릭 이벤트 리스너
+        //(라이브러리 사용) - 내용 변동 시 : 더보기 클릭 이벤트 리스너
         /*
 
         val readMoreTextView: ReadMoreTextView = binding.tvAgreeContent1
@@ -47,19 +38,25 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, TermsContentActivity1::class.java)
             startActivity(intent)
         }
-
         */
-        //마케팅 수신 동의 내용 (tvAgreeContent2)
-        binding.tvAgreeContent2.text = "마케팅수신정보 마케팅수신정보 마케팅수신정보 마케팅수신\n" +
-                "품고 기관과 있으며, 만물은 곳으로 운다. 사랑의 그들은 같이,\n" +
-                "청춘에서만 되려니와, 방지하는 있는가? 그들을 실현에 유소년\n" +
-                "에게서 봄바람이다. 따뜻한 바로 영원히 우리 커다란\n ㅁㅁㅁ\n aaaa\n ㅁㅁㅁ\n..."
 
+        //더보기 text 버튼 클릭 이벤트 리스너 (고정)
+        binding.btnViewMore1.setOnClickListener {
+            val intent = Intent(this, TermsContentActivity1::class.java)
+            startActivity(intent)
+        }
+
+        //마케팅 수신 동의 내용 (tvAgreeContent2)
         /*
          binding.tvAgreeContent2.setOnClickListener {
             startActivity(Intent(this, TermsContentActivity2::class.java))
         }
          */
+        //더보기 text 버튼 클릭 이벤트 리스너 (고정)
+        binding.btnViewMore2.setOnClickListener {
+            val intent = Intent(this, TermsContentActivity2::class.java)
+            startActivity(intent)
+        }
 
         //checkbox 모두 선택 시 버튼 활성화
         var if1Checked : Int = 0
@@ -90,6 +87,7 @@ class MainActivity : AppCompatActivity() {
                 binding.btnComleteAfter.visibility = View.INVISIBLE
             }
         }
+
         binding.btnComleteAfter.setOnClickListener{
             val intent = Intent(this, TermsContentActivity2::class.java)
             //**이 부분 약관 동의 완료 시 "000님 환영합니다" 있는 시작 화면으로 수정해야 함.**
