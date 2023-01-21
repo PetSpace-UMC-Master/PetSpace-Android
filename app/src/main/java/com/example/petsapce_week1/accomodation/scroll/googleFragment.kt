67f5e1152f45601de561465f3df930309cc2c196
chanu <cw8881@naver.com>
2023-01-21 21:38:16 +0900

    fix : 리뷰 프레그먼트 디자인 수정

--- app/src/main/java/com/example/petsapce_week1/accomodation/scroll/googleFragment.kt
+++ app/src/main/java/com/example/petsapce_week1/accomodation/scroll/googleFragment.kt
@@ -1,5 +1,6 @@
 package com.example.petsapce_week1.accomodation.scroll
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.petsapce_week1.databinding.FragmentGoogleBinding
+import com.example.petsapce_week1.loginrelated.LoginActivity
 
 class googleFragment : Fragment() {
 
@@ -19,8 +21,13 @@ class googleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewBinding = FragmentGoogleBinding.inflate(layoutInflater)
+
         //activity의 setcontentview가 아닌 return값을 주면된다.
 
+        viewBinding.imgGoogle.setOnClickListener {
+            val intent = Intent(context,LoginActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
