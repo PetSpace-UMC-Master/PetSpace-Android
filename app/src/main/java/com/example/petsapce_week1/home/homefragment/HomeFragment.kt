671b3f24975df696c53600fb2c6e1afcd7442246
chanu <cw8881@naver.com>
2023-01-26 03:03:40 +0900

    feat : edittext constraintlayout으로 구현

--- app/src/main/java/com/example/petsapce_week1/home/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/petsapce_week1/home/homefragment/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.petsapce_week1.home.homefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.petsapce_week1.GifActivity
 import com.example.petsapce_week1.R
 import com.example.petsapce_week1.databinding.FragmentHomeBinding
 
@@ -19,6 +21,16 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
+        //edittext가 아니라 constraint로 구현해야할듯
+        binding.editTextMainTop.setOnClickListener {
+            val intent = Intent(context,GifActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnEdittext.setOnClickListener {
+            val intent = Intent(context,GifActivity::class.java)
+            startActivity(intent)
+        }
 
 
         // Inflate the layout for this fragment
