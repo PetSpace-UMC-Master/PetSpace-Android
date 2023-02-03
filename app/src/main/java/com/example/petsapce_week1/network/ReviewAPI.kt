4b3629626923191f2c6c4d21fb9110a3634305a8
Channie <jinny102@naver.com>
2023-02-03 13:37:32 +0900

    FIX: 리뷰-이미지 서버 연결 중

--- app/src/main/java/com/example/petsapce_week1/network/ReviewAPI.kt
+++ app/src/main/java/com/example/petsapce_week1/network/ReviewAPI.kt
@@ -1,17 +1,21 @@
 package com.example.petsapce_week1.network
 
 import com.example.petsapce_week1.review.ReviewData
-import com.example.petsapce_week1.vo.accomo_datamodel.ReviewDTO
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ReviewAPI {
-   // @Multipart
+   @Multipart
     @POST("/app/reviews?reservationId=1/")
     fun post_reviews(
         //@Header("")
         //@Part jsonParams: ReviewDTO,
-        //@Part images: MultipartBody.Part
-        @Body jsonParams: ReviewDTO
+       @PartMap data: HashMap<String, RequestBody>,
+        @Part image: MultipartBody.Part?
+        //@Body jsonParams: ReviewDTO
     ): Call<ReviewData>
+
+  //  abstract fun post_reviews(jsonParams: ReviewDTO): Call<ReviewData>
 }
--- app/src/main/java/com/example/petsapce_week1/review/ReviewPostActivity.kt
+++ app/src/main/java/com/example/petsapce_week1/review/ReviewPostActivity.kt
@@ -1,8 +1,11 @@
 package com.example.petsapce_week1.review
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +14,15 @@ import com.example.petsapce_week1.TestMainActivity
 import com.example.petsapce_week1.databinding.ReviewCreateBinding
 import com.example.petsapce_week1.network.RetrofitHelper
 import com.example.petsapce_week1.network.ReviewAPI
-import com.example.petsapce_week1.vo.accomo_datamodel.ReviewDTO
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.BufferedSink
 import retrofit2.Retrofit
+import java.io.File
 
 
 class ReviewPostActivity : AppCompatActivity() {
@@ -24,16 +31,24 @@ class ReviewPostActivity : AppCompatActivity() {
     var api: ReviewAPI = retrofit.create(ReviewAPI::class.java)
     private var success_review_id: Int? = null
     private var review_rate: Int? = null
+    private var mediaPath: String ?= null
+
+    val content = binding.reviewInput.text.toString()
+
 
     private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode == RESULT_OK && it.data != null) {
             val uri = it.data!!.data
+            mediaPath = getRealPathFromURI(it.data?.data!!)
 
             Glide.with(this)
                 .load(uri)
                 .into(binding.selectedImage)
+            //val requestFile = RequestBody.create("image/*".toMediaTypeOrNull(), file)
+            //val body: MultipartBody.Part = MultipartBody.Part.createFormData("photo", "photo", requestFile)
 
+            //fileToUpload()
         }
     }
 
@@ -60,13 +75,23 @@ class ReviewPostActivity : AppCompatActivity() {
 
         binding.btnReviewCreate.setOnClickListener {
 
+            sendReviewRequest()
             // == 백엔드 통신 부분 ==
+            /*val file: File = File(mediaPath)
+            var inputStream: InputStream? = null try {
+            inputStream = getContext().getContentResolver().openInputStream(photoUri)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            val byteArrayOutputStream: ByteArrayOutputStream =
+                ByteArrayOutputStream() bitmap.compress Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream)   var requestBody: RequestBody? = create(MediaType.parse("image/jpg"), byteArrayOutputStream.toByteArray())   var uploadFile: Part? = createFormData.createFormData("postImg", file.getName(), requestBody)
             val content = binding.reviewInput.text.toString()
 
-            val data = ReviewDTO(content, review_rate)
+            val data = ReviewDTO(content, review_rate, fileToUpload())*/
 
 
-            api.post_reviews(data).enqueue(object : Callback<ReviewData> {
+/*            api.post_reviews(data, image).enqueue(object : Callback<ReviewData> {
                 override fun onResponse(
                     call: Call<ReviewData>,
                     response: Response<ReviewData>
@@ -86,7 +111,7 @@ class ReviewPostActivity : AppCompatActivity() {
                 override fun onFailure(call: Call<ReviewData>, t: Throwable) {
                     Log.d("리뷰 포스트", "failed")
                 }
-            })
+            })*/
         }
     }
 
@@ -99,8 +124,60 @@ class ReviewPostActivity : AppCompatActivity() {
         }
     }
 
+    private fun getRealPathFromURI(uri: Uri): String {
+        val buildName = Build.MANUFACTURER
+        if(buildName.equals("Xiaomi")) {
+            return uri.path.toString()
+        }
+
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        var cursor = contentResolver.query(uri, proj, null, null, null)
+
+        if(cursor!!.moveToFirst()) {
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+
+        return cursor.getString(columnIndex)
+    }
 
 
+/*    private fun fileToUpload() {
+        if (mediaPath != null) {
+            val file = File(mediaPath)
+            // image/jpeg 타입은 MIME 타입을 따르기 위함이다. 일반적인 String같은 경우 text/plain과 같이 쓴다.
+            val requestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            MultipartBody.Part.createFormData("images", file.name, requestBody)
+            // 우리 프로젝트에서는 이미지 파일이 없으면 null로 넘겨주기로 약속했기 때문에 이미지 경로가 없으면 null처리 해준다.
+        } else {
+            null
+        }
+    }*/
+
+    fun sendReviewRequest() {
+        val contentRequestBody : RequestBody = content.toPlainRequestBody()
+        val reviewRateRequestBody: RequestBody = review_rate.toString().toPlainRequestBody()
+        val textHashMap = hashMapOf<String, RequestBody>()
+        textHashMap["content"] = contentRequestBody
+        textHashMap["review_rate"] = reviewRateRequestBody
+
+        val file = File(mediaPath)
+        val bitmapRequestBody = bitmap?.let { BitmapRequestBody(it)}
+        val bitmapMultipartBody: MultipartBody.Part? =
+            if (bitmapRequestBody == null) null
+            else MultipartBody.Part.createFormData("reviewImages", file.name, bitmapRequestBody)
+
+        val response = api.addActivity(bitmapMultipartBody, textHashMap).awaitResponse()
+    }
+
+    inner class BitmapRequestBody(private val bitmap: Bitmap) : RequestBody() {
+        override fun contentType(): MediaType = "image/jpeg".toMediaType()
+        override fun writeTo(sink: BufferedSink) {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 99, sink.outputStream())
+        }
+    }
+
+    private fun String?.toPlainRequestBody() = requireNotNull(this).toRequestBody("text/plain".toMediaTypeOrNull())
 }
 
 /*    private fun openGallery() {
