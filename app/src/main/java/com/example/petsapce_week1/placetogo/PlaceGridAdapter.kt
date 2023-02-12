4cbb85f53989143ca6c70158a2237184a03172d0
MADElinessss <jyseen@naver.com>
2023-02-12 13:44:37 +0900

    {FIX} 함께 갈 곳 화면 다시 뜹니다

--- app/src/main/java/com/example/petsapce_week1/placetogo/PlaceGridAdapter.kt
+++ app/src/main/java/com/example/petsapce_week1/placetogo/PlaceGridAdapter.kt
@@ -3,16 +3,13 @@ package com.example.petsapce_week1.placetogo
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import androidx.fragment.app.FragmentManager
+import android.widget.ImageView
 import com.example.petsapce_week1.R
 import com.example.petsapce_week1.network.AccomoService
 import com.example.petsapce_week1.network.RetrofitHelper
@@ -20,10 +17,9 @@ import com.example.petsapce_week1.vo.FavoriteBackendResponse
 import kotlinx.android.synthetic.main.placetogo_grid_itemlist.view.*
 import retrofit2.Retrofit
 import java.io.Serializable
-import javax.xml.datatype.DatatypeFactory.newInstance
 
 
-class PlaceGridAdapter(val context: Context, var img_list:  Array<Int>, val accessToken: String, val fragmentManager: FragmentManager) : BaseAdapter() {
+class PlaceGridAdapter(val context: Context, var img_list:  Array<Int>, val accessToken: String) : BaseAdapter() {
 
     override fun getCount(): Int {
         return img_list.size
@@ -46,36 +42,52 @@ class PlaceGridAdapter(val context: Context, var img_list:  Array<Int>, val acce
 
     var accommoList = mutableListOf<FavoriteBackendResponse.Favorite>()
 
-    @SuppressLint("ViewHolder", "InflateParams")
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.placetogo_grid_itemlist, null)
+    @SuppressLint("ViewHolder", "InflateParams", "CutPasteId")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
 
-        view.menu_seoul.setImageResource(img_list[position])
+        Log.d("함께 갈 곳", "111")
+
+        //override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        //  val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.my_item_layout, parent, false)
+        //  val imageView = view.findViewById<ImageView>(R.id.my_image_view)
+        //  val image = images[position]
+        //  imageView.setImageResource(image)
+        //  return view
+        //}
+        val view : View = convertView ?: LayoutInflater.from(context).inflate(R.layout.placetogo_grid_itemlist, parent, false)
+        val imageView = view.findViewById<ImageButton>(R.id.menu_seoul)
+        val image = img_list[position]
+        imageView.setImageResource(image)
+
+
+        Log.d("함께 갈 곳", accessToken)
+        view?.menu_seoul?.setImageResource(img_list[position])
         Log.d("함께 갈 곳 어답터", img_list.toString())
 
-        val button = view.findViewById<ImageButton>(R.id.menu_seoul)
+        val button = view?.findViewById<ImageButton>(R.id.menu_seoul)
         var isLast : Boolean = false
         var page : Int = 0
 
-        while(!isLast){
-            button.setOnClickListener {
-                postaccessToken = "Bearer $accessToken"
-                api.getFavorites(postaccessToken, "SEOUL", page,4).enqueue(object : retrofit2.Callback<FavoriteBackendResponse>{
+        button?.setOnClickListener {
+            postaccessToken = "Bearer $accessToken"
+            api.getFavorites(postaccessToken, "SEOUL", page, 4)
+                .enqueue(object : retrofit2.Callback<FavoriteBackendResponse> {
                     override fun onResponse(
                         call: retrofit2.Call<FavoriteBackendResponse>,
                         response: retrofit2.Response<FavoriteBackendResponse>
                     ) {
                         Log.d("함께 갈 곳 성공", response.toString())
                         Log.d("함께 갈 곳", response.body().toString())
-                        if(response.body()?.result  != null){
+                        if (response.body()?.result != null) {
                             accommoList = response.body()?.result?.favorites?.toMutableList()!!
 
                             Log.d("함께 갈 곳 데이터2", "$accommoList")
-                            val nextScreenIntent = Intent(context, SeoulAccommoActivity::class.java).apply {
-                                putExtra("accommoList", accommoList as Serializable)
-                            }
+                            val nextScreenIntent =
+                                Intent(context, SeoulAccommoActivity::class.java).apply {
+                                    putExtra("accommoList", accommoList as Serializable)
+                                }
                             context.startActivity(nextScreenIntent)
-                            if(response.body()!!.result.isLast){
+                            if (response.body()!!.result.isLast) {
                                 isLast = true
                             }
                             // ** seoul activity 로 이동하면 되는 코드....
@@ -89,16 +101,15 @@ class PlaceGridAdapter(val context: Context, var img_list:  Array<Int>, val acce
                             Log.d("함께 갈 곳 서울 인텐t", "ㅇㅇ")
                             context.startActivity(intent)
                             Log.d("함께 갈 곳 서울 start", "ㅇㅇ")
-                        }
-                        else{
+                        } else {
 
                             // ** no place to go fragment 로 이동하면 되는 코드....
-                            val fragmentTransaction = fragmentManager.beginTransaction()
-                            val newFragment = NoPlaceToGoFragment()
-                            fragmentTransaction.add(R.id.placetogoLayout, newFragment)
-                            fragmentTransaction.addToBackStack(null)
-                            fragmentTransaction.commit()
-                            Log.d("함께 갈 곳 no", "noplacetogo")
+//                            val fragmentTransaction = fragmentManager.beginTransaction()
+//                            val newFragment = NoPlaceToGoFragment()
+//                            fragmentTransaction.add(R.id.placetogoLayout, newFragment)
+//                            fragmentTransaction.addToBackStack(null)
+//                            fragmentTransaction.commit()
+//                            Log.d("함께 갈 곳 no", "noplacetogo")
 
 //                        val intent = Intent(context, NoPlaceToGoFragment::class.java)
 //                        Log.d("함께 갈 곳 인텐t", "ㅇㅇ")
@@ -131,19 +142,15 @@ class PlaceGridAdapter(val context: Context, var img_list:  Array<Int>, val acce
 //                    val intent = Intent(context, NoPlaceToGoFragment::class.java)
 //                    context.startActivity(intent)
                         // ** no place to go fragment 로 이동하면 되는 코드....
-                        val fragmentTransaction = fragmentManager.beginTransaction()
-                        val newFragment = NoPlaceToGoFragment()
-                        fragmentTransaction.add(R.id.placetogoLayout, newFragment)
-                        fragmentTransaction.addToBackStack(null)
-                        fragmentTransaction.commit()
-                        Log.d("함께 갈 곳 no", "noplacetogo")
+//                        val fragmentTransaction = fragmentManager.beginTransaction()
+//                        val newFragment = NoPlaceToGoFragment()
+//                        fragmentTransaction.add(R.id.placetogoLayout, newFragment)
+//                        fragmentTransaction.addToBackStack(null)
+//                        fragmentTransaction.commit()
+//                        Log.d("함께 갈 곳 no", "noplacetogo")
                     }
                 })
-
-            }
-            page+=1
         }
         return view
     }
-
 }
